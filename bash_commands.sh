dd4d079882e011ed918fa53b03abe1b1dfc5005b
Mohamed Gomaa <66668949+mo7903@users.noreply.github.com>
2023-09-21 12:51:09 +0300

    Update bash_commands.sh for all OS

--- bash_commands.sh
+++ bash_commands.sh
@@ -1,18 +1,18 @@
 # Start Zookeeper server
-cd /usr/local/Cellar/kafka/3.5.1/libexec/
+cd {KAFKA_HOME/libexec}
 bin/zookeeper-server-start.sh config/zookeeper.properties
 
 # Start Kafka Broker
-cd /usr/local/Cellar/kafka/3.5.1/libexec/
+cd {KAFKA_HOME/libexec}
 bin/kafka-server-start.sh config/server.properties
 
 # Create Kafka Topic and Producer server
-cd /usr/local/Cellar/kafka/3.5.1/libexec/
+cd {KAFKA_HOME/libexec}
 bin/kafka-console-producer.sh --broker-list localhost:9092 --topic cookie_logs
 
 # OPTIONAL FOR TESTING: Create Kafka Consumer to receive data
-cd /usr/local/Cellar/kafka/3.5.1/libexec/
+cd {KAFKA_HOME/libexec}
 bin/kafka-console-consumer.sh --bootstrap-server localhost:9092 --topic cookie_logs --from-beginning
 
 # Start Apache NiFi
-/usr/local/Cellar/nifi/1.23.2/libexec/bin/nifi.sh start
+{NIFI_HOME}/libexec/bin/nifi.sh start
